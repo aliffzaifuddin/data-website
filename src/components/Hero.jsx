48c85e9ba6108802073ecfd3c3a520fd313c5bd6
Aliff Zaifuddin <aliffzaifuddin97@gmail.com>
2024-02-03 16:09:59 +0000

    update navbar and hero components

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ReactTyped } from "react-typed";
+
+const Hero = () => {
+  return (
+    <div className='text-white'>
+      <div className='max-w-[800px] w-full h-screen mt-[-96px] mx-auto text-center flex flex-col justify-center'>
+        <p className='uppercase text-[#00df9a] font-bold p-2'>Growing with Data Analytics</p>
+        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data.</h1>
+        <div>
+          <p className='md:text-5xl sm:text-4xl text-xl font-bold'>Fast, flexible financing for</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
