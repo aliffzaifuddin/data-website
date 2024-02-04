853a373abd9754931bab3f98bb90f3d2c28ee230
Aliff Zaifuddin <aliffzaifuddin97@gmail.com>
2024-02-04 17:05:45 +0000

    added few components and images

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-import React from "react"
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Analytics from "./components/Analytics"
+import Newsletter from "./components/Newsletter"
+import Cards from "./components/Cards"
+import Footer from "./components/Footer"
 
 function App() {
   
@@ -7,6 +11,11 @@ function App() {
     <>
       <h1 className="">
         <Navbar />
+        <Hero />
+        <Analytics />
+        <Newsletter />
+        <Cards />
+        <Footer />
       </h1>
     </>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,9 +7,20 @@ const Hero = () => {
       <div className='max-w-[800px] w-full h-screen mt-[-96px] mx-auto text-center flex flex-col justify-center'>
         <p className='uppercase text-[#00df9a] font-bold p-2'>Growing with Data Analytics</p>
         <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data.</h1>
-        <div>
-          <p className='md:text-5xl sm:text-4xl text-xl font-bold'>Fast, flexible financing for</p>
+        <div className='flex justify-center items-center'>
+          <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>Fast, flexible financing for</p>
+          <ReactTyped 
+            className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
+            strings={['B2B', 'B2C', 'SAAS']} 
+            typeSpeed={120}
+            backSpeed={120}
+            loop
+          />
         </div>
+        <p className='md:text-2xl text-xl font-bold text-gray-500'>
+          Monitor your data analytics to increase revenue for B2B, B2C & SAAS platforms. 
+        </p>
+        <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
       </div>
     </div>
   )
