83b0ff4759a87c7f6566909fa4d448c1a5f1eeba
Aliff Zaifuddin <aliffzaifuddin97@gmail.com>
2024-02-05 00:09:09 +0000

    fix styling

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Single from '../assets/single.png'
 import Double from '../assets/double.png'
 import Triple from '../assets/triple.png'
@@ -6,7 +5,7 @@ import Triple from '../assets/triple.png'
 const Cards = () => {
   return (
     <div className='w-full py-[10rem] px-4 bg-white '>
-      <div className='max-w-[2140px] mx-auto grid md:grid-cols-3 gap-8'>
+      <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
         <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300 text-center'>
           <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="" />
           <h2 className='text-2xl font-bold py-8'> Personal </h2>
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 
 const Newsletter = () => {
   return (
@@ -15,7 +15,8 @@ const Newsletter = () => {
             <input className='p-3 flex w-full rounded-md text-black' type='email' placeholder='Enter Email' />
             <button className='bg-[#00df9a] text-black w-[200px] rounded-md font-medium my-6 ml-4 py-3 '> Notify Me! </button>
           </div>
-          <p>We care about the protection of your data. Read our <span className='text-[#00df9a]'>Privacy Policy.</span></p>
+          <p>We care about the protection of your data. </p>
+          <p>Read our <span className='text-[#00df9a]'>Privacy Policy.</span></p>
         </div>
       </div>      
     </div>
